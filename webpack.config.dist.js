a1db48014d952ea8ab7c8c26efb22aa8edde015e
siminino <victor.areas@corp.globo.com>
2017-08-01 10:56:16 -0300

    starting new build config

---
+++ webpack.config.dist.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/portal.js'
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: path.join(__dirname, './dist'),
+    filename: 'portal.js',
+    library: "portal",
+    libraryTarget: "umd"
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+  devtool: "source-map",
+  devServer: {
+    inline: true,
+    contentBase: "./"
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+      {
+        test: /\.json$/,
+        loader: "json"
+      }
+    ]
+  }
+};
