2b3441836c5c1c05d91da21d3f14fb691d3839a4
siminino <victor.areas@corp.globo.com>
2017-08-01 15:19:18 -0300

    configure new example

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: __dirname,
     publicPath: "/",
-    filename: "demo/bundle.js"
+    filename: "examples/bundle.js"
   },
   devtool: "source-map",
   devServer: {
